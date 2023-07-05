1b118b30255ceed8137b2abb0bcab24b18ceec84
Moyasi Ginko <mrsiyam61@gmail.com>
2023-07-05 16:09:40 +0600

    Add reserve anime funtion

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,16 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchDetails } from '../redux/features/Details/detailsSlice';
+import {
+  reserveAnime,
+  cancelReservation,
+} from '../redux/features/Animes/animesSlice';
 
 const DetailsPage = () => {
   const { animeId } = useParams();
   const dispatch = useDispatch();
   const { data, isLoading, error } = useSelector((state) => state.details);
+  const [isReserved, setReserved] = useState(
+    localStorage.getItem(`reserved_${animeId}`) === 'true'
+  );
 
   useEffect(() => {
     dispatch(fetchDetails(animeId));
   }, [dispatch, animeId]);
+
+  const handleReserveAnime = () => {
+    dispatch(reserveAnime(animeId));
+    setReserved(true);
+  };
+
+  const handleCancelReservation = () => {
+    dispatch(cancelReservation(animeId));
+    setReserved(false);
+  };
 
   if (isLoading) {
     return <div>Loading anime details...</div>;
@@ -34,12 +51,32 @@ const DetailsPage = () => {
   return (
     <div>
       <h1>{title}</h1>
+      {isReserved ? <span className="anime-reserved">Reserved</span> : <></>}
       <div>
         <img src={images?.jpg?.image_url} alt={title} />
       </div>
       <div>{synopsis}</div>
       <div>
         <Link to={`/anime/${animeId}/details`}>See more details</Link>
+      </div>
+      <div>
+        {isReserved ? (
+          <button
+            type="button"
+            className="anime-cancel-btn"
+            onClick={handleCancelReservation}
+          >
+            Cancel Reservation
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="anime-reserve-btn"
+            onClick={handleReserveAnime}
+          >
+            Reserve Anime
+          </button>
+        )}
       </div>
     </div>
   );
